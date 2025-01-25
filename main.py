b37f2154cc729b7ee8c01992c2990de37bb94c50
Abdullahi Mohammmed Ali <abdumok19@gmail.com>
2025-01-25 17:37:50 +0300

    complete the program logic and add some ascii logo

--- main.py
+++ main.py
@@ -1,11 +1,36 @@
 import os
+import string
+import ascii_art
+
+letters = string.ascii_lowercase
+
+
 # Create function to clear the screen
 def clear():
     os.system("cls" if os.name == "nt" else "clear")
 
 # Create encryption function:
-def encryption(string, encryption_key):
-    string = input("Enter the sentence you want to encrypt: ")
-    encryption_key = ""
-    while not encryption_key.isdigit():
-        encryption_key = input("Enter the encryption key: ")
+def encryption(sentence, encryption_key):
+    encryption_letter = ""
+    for char in sentence:
+        if char.isalpha():
+            char_index = letters.index(char.lower())
+            encrypt_char = letters[char_index + int(encryption_key)]
+            if char == char.capitalize():
+                encryption_letter += encrypt_char.upper()
+            else:
+                encryption_letter += encrypt_char
+        else:
+            encryption_letter += char
+    print(encryption_letter)
+
+print(ascii_art.logo)
+original_sentence = input("Enter the sentence you want to encrypt: ")
+shift_key = input("Enter the encryption key: ")
+
+encryption(original_sentence,shift_key)
+
+
+
+
+
